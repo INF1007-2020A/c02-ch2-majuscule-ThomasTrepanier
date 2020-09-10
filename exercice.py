7b8e1236d5fb9de7f4ad5f7842ce79f02cb522a5
ThomasTrepanier <thomtrepanier@gmail.com>
2020-09-10 22:09:57 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     resultat = ''
-    dist_entre_min_et_maj = 32
+    dist_entre_min_et_maj = ord('a') - ord('A')
     LIMIT_MAJ = 90
     for lettre in mot:
         # TODO completer la fonction ici
